1e6c0a2eeabcc5ec6bbf8962ff14fc62aac55bab
admin <admin@CHETUIWK476>
2022-03-01 17:44:07 +0530

    Add project files.

---
+++ satyamweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace satyamweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
